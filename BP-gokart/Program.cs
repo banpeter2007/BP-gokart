105317909d5f2f746a51ed4b65de6dd8f658c6c6
banpeter2007 <ban.peter@diak.szbi-pg.hu>
2025-09-28 23:25:55 +0200

    otthoni_progress_2week
    part 2

--- BP-gokart/Program.cs
+++ BP-gokart/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace BP_gokart
 {
@@ -34,6 +35,9 @@ namespace BP_gokart
         }
     }
 
+    #endregion
+
+    #region 2. feladat: Versenyzők generálása
     public class Versenyzok
     {
         private static List<string> veznevek;   // Statikus listák – fájlokból csak egyszer olvassuk be
@@ -66,7 +70,6 @@ namespace BP_gokart
             int day = rnd.Next(1, DateTime.DaysInMonth(year, month) + 1);
             szuldatum = new DateTime(year, month, day);
 
-            int age = DateTime.Now.Year - szuldatum.Year;
             felnott = szuldatum.AddYears(18) <= DateTime.Now;
 
             string teljesnev = $"{veznev}{kernev}";
@@ -81,8 +84,42 @@ namespace BP_gokart
             Console.WriteLine($"{veznev} {kernev}|Születési dátum:{szuldatum:yyyy.MM.dd.}|Felnőtt:{(felnott ? "Igen" : "Nem")}|Azonosító:{azon}|Email:{email}");
         }
     }
-        #endregion
-        internal class Program
+    #endregion
+
+    #region 3. feladat: Pályabérlés szabályai, foglalások időpontjai
+    public class Foglalasok
+    {
+        public Versenyzok versenyzo;
+        public DateTime kezdes;
+        public int idotartam;
+        public DateTime vege => kezdes.AddHours(idotartam);
+
+        private static Random rnd = new Random();
+
+        public Foglalasok(Versenyzok v)
+        {
+            versenyzo = v;
+            idotartam = rnd.Next(1, 2 + 1);
+            
+            DateTime kezdo_nap = DateTime.Today;
+            DateTime zaro_nap = new DateTime(2027, 12, 31);
+            int napok_szama = (zaro_nap - kezdo_nap).Days;
+            DateTime randomnap = kezdo_nap.AddDays(rnd.Next(napok_szama + 1));
+
+            int ora = rnd.Next(8, 19 + 1) - idotartam;  // 8 és 19 óra között kezdődhet, de figyelembe kell venni az időtartamot is
+
+            kezdes = new DateTime(randomnap.Year, randomnap.Month, randomnap.Day, ora, 0, 0);
+        }
+
+        public void Kiir()
+        {
+            Console.WriteLine($"{versenyzo.veznev} {versenyzo.kernev}|{kezdes:yyyy.MM.dd. HH:mm} - {vege:HH:mm}|{idotartam} órás foglalás");
+        }
+
+    }
+    #endregion
+
+    internal class Program
     {
         #region fejlec
         static void fejlec()
@@ -127,6 +164,20 @@ namespace BP_gokart
             */
 
             Console.WriteLine($"Eddig {darab} versenyző regisztrált be az összes adatával a foglalási naplóba.");
+
+            List<Foglalasok> foglalasok = new List<Foglalasok>();
+
+            foreach (var v in versenyzok)
+            {
+                foglalasok.Add(new Foglalasok(v));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Foglalások:");
+            foreach (var f in foglalasok)
+            {
+                f.Kiir();
+            }
 
             Console.WriteLine();
             Console.WriteLine("Kilépéshez nyomja meg az ENTER billentyűt.");
